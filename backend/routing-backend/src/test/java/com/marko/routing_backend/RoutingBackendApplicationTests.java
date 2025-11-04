969a670430e141c5d9f657d78b98b46d0f35e91b
Marko Vrljes <markovrljes11@gmail.com>
2025-11-04 13:56:49 -0500

    mvn updates

---
+++ backend/routing-backend/src/test/java/com/marko/routing_backend/RoutingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marko.routing_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
